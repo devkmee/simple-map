df5b7a8033d00c36da095d4e99aca121004191ac
devkmee <devkmee@gmail.com>
2024-12-21 16:56:10 +0900

    Chore: 포트변경, tailwindCss 설정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Main = () => {
 	return (
 		<div>
-			<h1 className='bg-purple-400 text-white p-4'>Main Page</h1>
+			<h1 className='bg-purple-400 text-white p-4'>왜쨰서</h1>
 			<p>Welcome to the main page</p>
 		</div>
 	);
