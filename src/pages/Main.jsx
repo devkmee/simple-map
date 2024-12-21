221995e37f6cffc41e2597d1229ec36995e1972c
devkmee <devkmee@gmail.com>
2024-12-21 16:33:15 +0900

    Chore : 프로젝트 생성

---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+	return (
+		<div>
+			<h1 className='bg-purple-400 text-white p-4'>Main Page</h1>
+			<p>Welcome to the main page</p>
+		</div>
+	);
+};
+
+export default Main;
